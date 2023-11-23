c6aa40075e22d3d2158ba97a556f37dc0d8702bf
hpramanik <hpramanik@outlook.in>
2023-11-23 12:08:11 +0530

    Easy list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import { IncrementApp } from './components/02_task.simple.increment.component/increment.app.tsx';
+import { ListComponent } from './components/common/list.component';
 
 function App() {
+  const listItems: Array<{ id: string; name: string; url?: unknown }> = [
+    { id: '1', name: 'Ram' },
+    { id: '2', name: 'Sam' },
+    { id: '3', name: 'Pam' },
+  ];
+
+  const cellStyle: React.CSSProperties = {
+    textAlign: 'center',
+    border: 'black',
+    borderStyle: 'ridge',
+  };
   return (
-    <div>
-      {/* <ComponentWithCreateElement />
-      <ComponentWithTSX /> */}
-      <IncrementApp />
-    </div>
+    <ListComponent
+      style={{
+        width: '100%',
+        textAlign: 'center',
+        border: 'black',
+        borderStyle: 'groove',
+      }}
+      data={listItems}
+      cols={[
+        {
+          key: 'id',
+          label: 'EMP ID',
+          style: cellStyle,
+          headerStyle: cellStyle,
+        },
+        {
+          key: 'name',
+          label: 'NAME',
+          style: cellStyle,
+          headerStyle: cellStyle,
+        },
+        {
+          key: 'url',
+          label: 'PROFILE',
+          render({ id, name }) {
+            return (
+              <a href="#">
+                https://www.verizon.com/profiles/{id}-{name}
+              </a>
+            );
+          },
+          style: cellStyle,
+          headerStyle: cellStyle,
+        },
+      ]}
+      rowUniqueIdentifierColName="id"
+    />
   );
 }
 
